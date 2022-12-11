f9c83e81317c8a45b214b564b34e82f067fd2784
Vendula Kudejova <vendula.kudejova@gmail.com>
2022-12-11 11:03:36 +0100

    add helper functions to the server file

--- server.js
+++ server.js
@@ -39,3 +39,59 @@ app.get('/game', (req, res) => {
 app.get('/questions', (req, res) => {
     res.sendFile(__dirname + '/public/manage-questions.html');
 })
+
+// PORT LISTENING
+
+app.listen(port, () => {
+    console.log(`The server is up and running on ${port}`);
+});
+
+// HELPER FUNCTIONS
+
+// querying database
+
+function queryDb(sqlQuery, valuesArr) {
+    return new Promise((resolve, reject) => {
+        conn.query(sqlQuery, valuesArr, (err, result) => {
+            if(err) {
+                return reject('DATABASE ERROR');
+            } else {
+                return resolve(result);
+            }
+        })
+    }) 
+}
+
+// getting random question
+
+async function getRandomQuestion() {
+    let questions = await queryDb(`SELECT * FROM questions`);
+    let questionId = Math.ceil(Math.random() * questions.length);
+    let question = questions[questionId - 1];
+    let answers = await queryDb(`SELECT * FROM answers WHERE question_id = ?`, [question.id]);
+    return { question, answers };
+}
+
+// adding new question
+
+async function addQuestion(question, answers) {  
+    let arrAnswers = [answers[0].answer_1, answers[1].answer_2, answers[2].answer_3, answers[3].answer_4]
+    let arrIsCorrect = answers.map(answer => answer.is_correct);
+
+    let insert = await queryDb('INSERT INTO questions (question) VALUES (?)', [question]);
+    let questionId = insert.insertId;
+    for(let i = 0; i < answers.length; i++) {
+        await queryDb(`INSERT INTO answers (question_id, answer, is_correct) VALUES (?, ?, ?)`, [questionId, arrAnswers[i], arrIsCorrect[i]]);
+    }
+    return await queryDb(`SELECT question, question_id, answer FROM questions JOIN answers ON questions.id = question_id WHERE questions.id = ? AND is_correct = ?`, [questionId, 1]);
+}
+
+// deleting question
+
+async function deleteQuestion(id) {
+    await queryDb(`DELETE FROM answers WHERE question_id = ?`, [id]);
+    await queryDb(`DELETE FROM questions WHERE id = ?`, [id]);
+    return {
+        message: 'The question and its answers were deleted.'
+    }
+}

3b8a77df6a5617cc421c5b108266bc905f2c548d
Vendula Kudejova <vendula.kudejova@gmail.com>
2022-12-11 11:12:57 +0100

    add helper functions to the main.js file

---
+++ public/main.js
@@ -0,0 +1,92 @@
+'use strict';
+
+// HELPER FUNCTIONS
+
+// QUESTION
+
+async function fetchRandomQuestion() {
+    let response = await fetch('/api/game');
+    return await response.json();
+}
+
+function renderQuestion() {
+    let questionSection = document.querySelector('.question');
+    let mainQuestion = document.createElement('h3');
+    let answer1 = document.createElement('button');
+    let answer2 = document.createElement('button');
+    let answer3 = document.createElement('button');
+    let answer4 = document.createElement('button');
+
+    let answers = document.createElement('div');
+    answers.classList.add('answers');
+
+    answers.appendChild(answer1);
+    answers.appendChild(answer2);
+    answers.appendChild(answer3);
+    answers.appendChild(answer4);
+
+    questionSection.appendChild(mainQuestion);
+    questionSection.appendChild(answers);
+}
+
+function markCorrectAnswer(answersFromData, layoutAnswers) {
+    return new Promise(resolve => {
+        for(let i = 0; i < answersFromData.length; i++) {
+            if(answersFromData[i].is_correct == 1) {
+                layoutAnswers[i].setAttribute('style', 'background-color: #27AE60');
+            }
+        } 
+    })      
+}
+
+// SCORE
+
+function setScore(score) {
+    let currentScore = document.querySelector('.score-count');
+    localStorage.setItem('score', score);
+    currentScore.textContent = score;
+}
+
+function changeScore(plusOrMinus) {
+    let currentScore = parseInt(localStorage.getItem('score'));
+    currentScore += (plusOrMinus);
+    setScore(currentScore);
+}
+
+function setCurrentScore() {
+    let currentScore = parseInt(localStorage.getItem('score'));
+    setScore(currentScore)
+}
+
+function resetScore() {
+    let currentScore = document.querySelector('.score-count');
+    localStorage.setItem('score', 0);
+    currentScore.textContent = 0;
+}
+
+let resetBtn = document.querySelector('.reset');
+
+resetBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    resetScore()
+})
+
+// TIMEOUT
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function reload() {
+    return new Promise(resolve => location.reload())
+}
+
+async function waitForCorrectAnswer(ms, answersFromData, layoutAnswers) {
+    await timeout(ms);
+    await markCorrectAnswer(answersFromData, layoutAnswers)
+}
+
+async function waitForReload(ms) {
+    await timeout(ms);
+    await reload()
+}
